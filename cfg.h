219e18dfe4604fcedcd0e93cb71955ad8b54d085
johnkim6823 <82578738+johnkim6823@users.noreply.github.com>
2022-12-08 10:15:06 +0900

    Update cfg.h  by MJ 2022.12.08

--- cfg.h
+++ cfg.h
@@ -4,8 +4,10 @@
 /*------ CAMERA_SETTINGS ------- */
 #define DEFAULT_WIDTH 1280
 #define DEFAULT_HEIGHT 720
+
 /*------ WINDOW_NAME ------ */
-#define WHOLE_IMG	"Webcam"
+#define GAME_IMG	"GAME"
+#define WEBCAM_IMG	"Webcam"
 #define FACE_IMG	"FACE"
 
 /*------ CASCADE_DIR -------*/
@@ -14,19 +16,26 @@
 
 /*------ INITALIZING ------*/
 #define LAMPING_TIME 20
+#define FACE_STORAGE_SIZE 30
 
 /*------  COLOR ------*/
 #define EYE_COLOR Scalar(0,0,255)
-#define FOCUS_COLOR Scalar(0,0,0)
-
+#define CURSOR_COLOR Scalar(0, 0,255)
 
 /*------  GAME ------*/
 #define GAME_IMG "GAME_IMAGE.jpg"
 
+cv::Point ANSWER_POINT1(685, 275);
+cv::Point ANSWER_POINT2(715, 335);
+
 
 class GAME {
+	cv::Rect ANSWER;
+
+
 public:
 	int GAME_INIT();
+	int GAME_PLAY(cv::Point& CURSUR, cv::Point& CURSUR_EX);
 };
 
 class PLAYER {
@@ -34,6 +43,11 @@ public:
 	int open_camera();
 	int open_cascade();
 	void lamping_time();
+	void detect_Eyes(cv::Mat& PLAYER_FOCUS, cv::Mat& GAME_FRAME);
 };
+
+/*------  etc ------*/
+#define BORDER_LINE "------------------------------"
+
 
 #endif
